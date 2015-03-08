91fc6a2caaf9753587c25f8083d6ea1641c2dec8
Ahmed <ahmedfay@hotmail.com>
2015-03-08 15:57:29 -0400

    Update routes for Jobs and Companies

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
 
+  root 'home#index'
   get 'companies/index'
 
   devise_for :users, controllers: { registrations: "registrations" }
-  root 'home#index'
 
-  resources :jobs do
-    resources :companies, only: [:new, :create ]
-      # resources :users, only: [:new, :create, :show, :update]
-
-  end
+  resources :users,     only: [:new, :create, :show, :update]
+  resources :companies, only: [:index, :new, :create, :show, :edit ]
+  resources :jobs,      only: [:index, :new, :create, :show, :edit ]
 end
